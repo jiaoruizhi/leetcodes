38b57c3a588c43d6d74dc96a136769c829f4ad3c
JiaoRuiZhi <13636597750@163.com>
2023-12-10 18:01:02 +0800

    2023 12 10
    jiaoruizhi

--- src/main/java/code202312/code_0070_climbStairs.java
+++ src/main/java/code202312/code_0070_climbStairs.java
@@ -26,11 +26,10 @@ public class code_0070_climbStairs {
 
   public int climbStairs(int n) {
     int[] cache = new int[n + 1];
-    cache[0] = 0;
+    cache[0] = 1;
     cache[1] = 1;
-    cache[2] = 1;
 
-    for (int i = 3; i <= n; i++) {
+    for (int i = 2; i <= n; i++) {
       cache[i] = cache[i - 1] + cache[i - 2];
     }
     return cache[n];
