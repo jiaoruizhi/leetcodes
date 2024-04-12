a94aaa315a9f238d8cadb9440dfa8cc9a969ac02
JiaoRuiZhi <13636597750@163.com>
2024-04-12 10:53:19 +0800

    2024  04  12
    jiao rui zhi

--- src/main/java/code202309/code_0020_isValid.java
+++ src/main/java/code202309/code_0020_isValid.java
@@ -2,7 +2,6 @@ package code202309;
 
 
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Stack;
--- src/main/java/wait/code_0101_isSymmetric.java
+++ src/main/java/wait/code_0101_isSymmetric.java
@@ -1,5 +1,9 @@
 package wait;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 import utils.*;
 import utils.dto.TreeNode;
 
@@ -35,18 +39,56 @@ import utils.dto.TreeNode;
 *
 * */
 public class code_0101_isSymmetric {
-    public static void main(String[] args) {
-        TreeNode root  = utils.getTree();
-        boolean ret = isSymmetric(root);
-        System.out.println(ret);
+  public static void main(String[] args) {
+    code_0101_isSymmetric body = new code_0101_isSymmetric();
+
+    // 1,2,2,3,4,4,3
+    // 1,2,2,null,3,null,3
+    TreeNode root = utils.getTree();
+    boolean ret = body.isSymmetric(root);
+    System.out.println(ret);
+
+  }
+
+  Queue<TreeNode> queue = new LinkedList<>();
+  Stack<TreeNode> stack = new Stack<>();
+
+  public boolean isSymmetric(TreeNode root) {
+
+    if (root == null) {
+
+      return true;
 
     }
 
-    public static boolean isSymmetric(TreeNode root) {
+    //调用递归函数，比较左节点，右节点
+
+    return dfs(root.left, root.right);
+
+  }
 
 
-        return true;
+
+  boolean dfs(TreeNode left, TreeNode right) {
+    //递归的终止条件是两个节点都为空
+    //或者两个节点中有一个为空
+    //或者两个节点的值不相等
+    if (left == null && right == null) {
+      return true;
     }
 
+    if (left == null || right == null) {
+      return false;
+    }
+
+    if (left.val != right.val) {
+      return false;
+    }
+
+    //再递归的比较 左节点的左孩子 和 右节点的右孩子
+    //以及比较  左节点的右孩子 和 右节点的左孩子
+    return dfs(left.left, right.right) && dfs(left.right, right.left);
+
+  }
 
 }
