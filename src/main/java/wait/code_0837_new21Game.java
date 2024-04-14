bee9b0beae6472b8ff147f87f289bf82fb3865dd
JiaoRuiZhi <13636597750@163.com>
2024-04-14 15:40:35 +0800

    2024  04  13
    jiao rui zhi

--- src/main/java/wait/code_0837_new21Game.java
+++ src/main/java/wait/code_0837_new21Game.java
@@ -1,12 +1,15 @@
 package wait;
 
 
+import java.util.Scanner;
+
 /*
 *
 *
 爱丽丝参与一个大致基于纸牌游戏 “21点” 规则的游戏，描述如下：
 
-爱丽丝以 0 分开始，并在她的得分少于 K 分时抽取数字。 抽取时，她从 [1, W] 的范围中随机获得一个整数作为分数进行累计，其中 W 是整数。 每次抽取都是独立的，其结果具有相同的概率。
+爱丽丝以 0 分开始，并在她的得分少于 K 分时抽取数字。
+抽取时，她从 [1, W] 的范围中随机获得一个整数作为分数进行累计，其中 W 是整数。 每次抽取都是独立的，其结果具有相同的概率。
 
 当爱丽丝获得不少于 K 分时，她就停止抽取数字。 爱丽丝的分数不超过 N 的概率是多少？
 
@@ -30,12 +33,20 @@ package wait;
 *
 * */
 public class code_0837_new21Game {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    code_0837_new21Game body = new code_0837_new21Game();
+    Scanner sc = new Scanner(System.in);
 
-    }
+    int N = sc.nextInt();
+    int K = sc.nextInt();
+    int W = sc.nextInt();
+
+    System.out.println(body.new21Game(N, K, W));
+
+  }
 
 
-    public static double new21Game(int N, int K, int W) {
-        return 0;
-    }
+  public double new21Game(int N, int K, int W) {
+    return 0;
+  }
 }
