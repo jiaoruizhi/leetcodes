a978e2ecbaf46384dfe23187149049e171f96162
JiaoRuiZhi <13636597750@163.com>
2024-04-15 18:06:05 +0800

    2024  04  13
    jiao rui zhi

--- src/main/java/code202404/code_face0801_waysToStep.java
+++ src/main/java/code202404/code_face0801_waysToStep.java
@@ -8,7 +8,6 @@ https://leetcode.cn/problems/three-steps-problem-lcci/description/
 三步问题。有个小孩正在上楼梯，楼梯有n阶台阶，
 小孩一次可以上1阶、2阶或3阶。实现一种方法，计算小孩有多少种上楼梯的方式。结果可能很大，你需要对结果模1000000007。
 * */
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class code_face0801_waysToStep {
@@ -24,23 +23,19 @@ public class code_face0801_waysToStep {
   }
 
   public int waysToStep(int n) {
+    if (n <= 2) {
+      return n;
+    }
     long[] cache = new long[n + 1];
 
-    for (int i = 0; i <= n; i++) {
-      cache[i] = 1;
-    }
+    cache[0] = 1;
+    cache[1] = 1;
+    cache[2] = 2;
 
-    for (int k = 2; k <= 3; k++) {
-      for (int i = k; i <= n; i++) {
-        long data = 0;
-        for (int j = 1; j <= k; j++) {
-          data += cache[i - j] % 1000000007;
-        }
-        cache[i] = data;
-      }
-      System.out.println(Arrays.toString(cache));
+    for (int i = 3; i <= n; i++) {
+      cache[i] = (cache[i - 1] + cache[i -2] + cache[i-3]) % 1000000007;
     }
-    return (int) (cache[n] % 1000000007);
+    return (int) cache[n];
   }
 
   public int waysToStep2(int n) {
