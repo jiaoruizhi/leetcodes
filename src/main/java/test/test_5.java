e97d41956780cb1e82383256792a61fefa7c9edc
jiaoruizhi <13636597750@163.com>
2024-08-02 17:27:25 +0800

    jiao rui zhi
    2024 08 02

--- src/main/java/test/test_5.java
+++ src/main/java/test/test_5.java
@@ -1,20 +1,23 @@
 package test;
 
 public class test_5 {
-  public static void main(String[] args) {
-    test_5 body = new test_5();
+    public static void main(String[] args) {
+        test_5 body = new test_5();
 
-    System.out.println(body.try_catch_return());
+        System.out.println(body.try_catch_return());
 
-  }
+        Long id = 684001577L;
+        System.out.println(id.intValue());
 
-  public int try_catch_return() {
-    try {
-      return 0;
-    } finally {
-      return 1;
     }
-  }
+
+    public int try_catch_return() {
+        try {
+            return 0;
+        } finally {
+            return 1;
+        }
+    }
 
 }
 
